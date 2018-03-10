35d9ad263317f26e7ce6cbdf170f080268eac0d0
Maria Duffy <maria.duffy@dvsa.gov.uk>
2018-03-10 19:10:53 +0000

    first calculation

--- js/resources.js
+++ js/resources.js
@@ -1,24 +1,47 @@
 // This is the json for the question object
 //
-// month {
-//	"month": int,	     		// question number
-//	"question": string,		// question
-//	"decision": boolean,		// yes or no
-//	"effectDuration": int,		// how many months of effect?
-//	"effectCreditReport": int,	// +/- on credit report number
-//	"effectMoneyTotal": int,	// +/- on money total
-//	"finalOutcome": string,		// comment in final report, can be null
-// } 
-//
-//
-//
 
-var monthQuestion {
-	"month": 1,
-	"question": "Do you register to vote?",
-	"decision": true,
-	"effectDuration": 0,
-	"effectCreditReport":150,
-	"effectMoneyTotal":0,
-	"finalOutcome": "Registering to vote makes credit score better.",
+var event = {
+	 newCar : {
+		"question":"Which new car do you want?",
+		"answer": [1,5,10],
+		"price": [1000,5000,10000],
+		"upfront": [1000,0,0],
+		"interest": [0,1.05,1.1], //interest per year
+		"recurring": function(b){
+			return b;
+		},
+// ((price-upfront)*interest)/paymentDuration = monthly repayment
+		"monthlyRepayment": // input: answer, return monthly repayment
+			function(a){
+// P = A/D
+// D = (((1+i)**n)-1)/(i(1+i**n))
+				//i = event.newCar.interest[a]/12.;
+				//D = (1+i)
+
+				return ((event.newCar.price[a]-event.newCar.upfront[a])
+					*event.newCar.interest[a])/event.newCar.paymentDuration; 
+			},
+		"paymentDuration": 36, //months
+		"notes":["yay","boo","really boo"],
+	} // newCar
+} // event
+
+function getIsRecurring() {
+	var t = event.newCar.recurring(true);
+	console.log(t);
+	
 }
+getIsRecurring();
+
+function getMonthlyRepayment(){
+	var t = event.newCar.monthlyRepayment(2);
+	console.log(t);
+}
+getMonthlyRepayment();
+
+function getReport(){
+	var t = event.newCar.notes[2];
+	console.log(t);
+}
+getReport();
