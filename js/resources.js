ee235277457ae4c88c8fffe43c87f02569a7509f
Maria Duffy <maria.duffy@dvsa.gov.uk>
2018-03-10 16:03:55 +0000

    inital question template

--- js/resources.js
+++ js/resources.js
@@ -1,13 +1,24 @@
 // This is the json for the question object
 //
 // month {
+//	"month": int,	     		// question number
+//	"question": string,		// question
+//	"decision": boolean,		// yes or no
+//	"effectDuration": int,		// how many months of effect?
+//	"effectCreditReport": int,	// +/- on credit report number
+//	"effectMoneyTotal": int,	// +/- on money total
+//	"finalOutcome": string,		// comment in final report, can be null
+// } 
 //
 //
 //
-//
-//
-//
-//}
-//
-//
-//
+
+var monthQuestion {
+	"month": 1,
+	"question": "Do you register to vote?",
+	"decision": true,
+	"effectDuration": 0,
+	"effectCreditReport":150,
+	"effectMoneyTotal":0,
+	"finalOutcome": "Registering to vote makes credit score better.",
+}
