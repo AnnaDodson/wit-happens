607a5ea889d41cf43348eb8c951a538603085e8b
Anna <anna@annadodson.co.uk>
2018-03-11 11:58:53 +0000

    Add changes

--- js/resources.js
+++ js/resources.js
@@ -24,26 +24,27 @@ function Answer(t, m, ti, ml){
 }
 
 var events = [
-   {
+	 {
     "title" : "new-car",
-    "speechPrompt" : "Hey! Look at my fancy new car!",
-    "question":"You really like Jones' new car and want to stop taking the bus. What do you do?",
-    "answerOptions": [
+		"speechPrompt" : "Hey! Look at my fancy new car!"
+		"question":"You really like Jones' new car and want to stop taking the bus. What do you do?",
+		"answerOptions": [
        { name : "A. Spend £1000 on buying a second hand car outright", value:  1},
        { name : "B. Spend £5000 and buy a car on finance", value:  5},
        { name : "C. Spend £10,000 and buy a car that is way better than Jones', on finance", value:  10}],
-    "price": [1000,5000,10000],
-    "getPrice": function(i){
+		"price": [1000,5000,10000],
+		"getPrice": function(i){
       return this.price[i];
     },
-    "upfront": [1000,0,0],
-    "interest": [0,0.05,0.1], //interest per year
-    "calculation":  function(a, t){
-        return a - 10;
-      },
+		"upfront": [1000,0,0],
+		"interest": [0,0.05,0.1], //interest per year
+		"calculation":	function(a, t){
+				return a - 10;
+			},
 
     "monthlyRepayment": // input: answer element, return monthly repayment
         function(a){
+
                 if (this.interest[a] != 0) {
                         i = this.interest[a]/12.;
                         n = this.paymentDuration;
@@ -55,13 +56,13 @@ var events = [
 
                 return  P;
         },
-    "paymentDuration": 36, //months
-    "notes":["yay","boo","really boo"],
+		"paymentDuration": 36, //months
+		"notes":["yay","boo","really boo"],
     "answer" : Answer
-  },
+	},
      {
     "title" : "new-TV",
-    "speechPrompt" : "Look how big my telly is! Want to watch the game?",
+    "speechPrompt" : "My telly is huge, want to watch the game at mine?",
     "question":"Your TV breaks, what do you do?",
     "answerOptions": [
        { name : "first ans", value:  1},
@@ -95,79 +96,6 @@ var events = [
     "notes":["yay","boo","really boo"],
     "answer" : Answer
   },
-   {
-    "title" : "new-blah ",
-    "question":"Which new thing do you want?",
-    "answerOptions": [
-       { name : "first ans", value:  1},
-       { name : "second ans", value:  5},
-       { name : "third ans", value:  10}],
-    "price": [1000,5000,10000],
-    "getPrice": function(i){
-      return this.price[i];
-    },
-    "upfront": [1000,0,0],
-    "interest": [0,0.05,0.1], //interest per year
-    "calculation":  function(a, t){
-        return a - 10;
-      },
-
-    "monthlyRepayment": // input: answer element, return monthly repayment
-        function(a){
-
-                if (this.interest[a] != 0) {
-                        i = this.interest[a]/12.;
-                        n = this.paymentDuration;
-                        D = (((1+i)**n)-1)/(i*(1+i)**n);
-                        A = this.price[a]- this.upfront[a];
-                        P = A/D
-                }
-                else {P = 0}
-
-                return  P;
-        },
-    "paymentDuration": 36, //months
-    "notes":["yay","boo","really boo"],
-    "answer" : Answer
-  },
-
-
-   {
-    "title" : "credit-card-application",
-    "speechPrompt" : "I've got a great cash back credit card from Amex.",
-    "question":"Your last two credit card application got declined, what are your next steps?",
-    "answerOptions": [
-       { name : "A. Apply for an Amex credit card", value:  10},
-       { name : "B. Apply for an M&S credit card, it might be easier to get", value:  5},
-       { name : "C. Contact Experian to get your credit report and understand your credit history", value:  1}],
-    "price": [0,0,0],
-    "getPrice": function(i){
-      return this.price[i];
-    },
-    "upfront": [0,0,0],
-    "interest": [0,0,0], //interest per year
-    "calculation":  function(a, t){
-        return a - 10;
-      },
-
-    "monthlyRepayment": // input: answer element, return monthly repayment
-        function(a){
-                if (this.interest[a] != 0) {
-                        i = this.interest[a]/12.;
-                        n = this.paymentDuration;
-                        D = (((1+i)**n)-1)/(i*(1+i)**n);
-                        A = this.price[a]- this.upfront[a];
-                        P = A/D
-                }
-                else {P = 0}
-
-                return  P;
-        },
-    "paymentDuration": 36, //months
-    "notes":["Bad","No, it won't be easier to get","Great! You need to understand your credit history."],
-    "answer" : Answer
-  },
-
    {
     "title" : "night-out",
     "question":"You're short of money this month, but you really want to go out and have fun with your mates. What do you do?",
@@ -204,13 +132,88 @@ var events = [
     "notes":["Well done!","Bad, missing payments is bad for your credit score","Bad, reaching your credit limit is very bad!"],
     "answer" : Answer
   },
+
+
+	 {
+    "title" : "credit-card-application",
+		"speechPrompt": "I've got a great cash back credit card from Amex."
+		"question":"Your last two credit card application got declined, what are your next steps?",
+		"answerOptions": [
+       { name : "A. Apply for an Amex credit card", value:  10},
+       { name : "B. Apply for an M&S credit card, it might be easier to get", value:  5},
+       { name : "C. Contact Experian to get your credit report and understand your credit history", value:  1}],
+		"price": [0,0,0],
+		"getPrice": function(i){
+      return this.price[i];
+    },
+		"upfront": [0,0,0],
+		"interest": [0,0,0], //interest per year
+		"calculation":	function(a, t){
+				return a - 10;
+			},
+
+    "monthlyRepayment": // input: answer element, return monthly repayment
+        function(a){
+
+                if (this.interest[a] != 0) {
+                        i = this.interest[a]/12.;
+                        n = this.paymentDuration;
+                        D = (((1+i)**n)-1)/(i*(1+i)**n);
+                        A = this.price[a]- this.upfront[a];
+                        P = A/D
+                }
+                else {P = 0}
+
+                return  P;
+        },
+		"paymentDuration": 36, //months
+		"notes":["Bad","No, it won't be easier to get","Great! You need to understand your credit history."],
+    "answer" : Answer
+	},
+
+	 {
+    "title" : "holiday",
+		"question":"You really want at least one holiday this year. What do you do?",
+		"speechPrompt" : "Oh, we're going to the Bahamas later this year, it's our third holiday this year.",
+		"answerOptions": [
+       { name : "A. Cut back on eating out and save for a holiday before you book it", value:  1},
+       { name : "B. Get a new credit card and book your holiday. You already have 3, one more should be fine.", value:  5},
+       { name : "C. Use your existing credit card, hoping it won't max out.", value:  10}],
+		"price": [0,0,0],
+		"getPrice": function(i){
+      return this.price[i];
+    },
+		"upfront": [0,0,0],
+		"interest": [0,0,0], //interest per year
+		"calculation":	function(a, t){
+				return a - 10;
+			},
+
+    "monthlyRepayment": // input: answer element, return monthly repayment
+        function(a){
+
+                if (this.interest[a] != 0) {
+                        i = this.interest[a]/12.;
+                        n = this.paymentDuration;
+                        D = (((1+i)**n)-1)/(i*(1+i)**n);
+                        A = this.price[a]- this.upfront[a];
+                        P = A/D
+                }
+                else {P = 0}
+
+                return  P;
+        },
+		"paymentDuration": 36, //months
+		"notes":["Well done!","Bad, too many credit cards","Bad, reaching your credit limit is very bad!"],
+    "answer" : Answer
+	},
   {
     "title" : "voterReg",
-    "speechPrompt" : "So, you moved house a while ago",
+    "speechPrompt" : "So, you moved house a while ago"
     "question":"Did you register to vote??",
     "answerOptions": [
        { name : "first ans", value:  1},
-       { name : "second ans", value:  5}],
+       { name : "second ans", value:  5},
     "price": [0,0],
     "getPrice": function(i){
       return this.price[i];
@@ -256,4 +259,3 @@ var outgoings = {
 var incomings = {
   wages : 1000
 };
-
